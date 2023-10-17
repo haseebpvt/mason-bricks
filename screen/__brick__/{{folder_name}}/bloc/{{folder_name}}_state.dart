7ec3a82063ea329b06646fffc068d22075f561f2
Abdul Haseeb <haseeb.a@2basetechnologies.com>
2023-10-17 23:44:09 +0530

    Saved files

--- screen/__brick__/{{folder_name}}/bloc/{{folder_name}}_state.dart
+++ screen/__brick__/{{folder_name}}/bloc/{{folder_name}}_state.dart
@@ -1,7 +1,7 @@
 part of '{{folder_name}}_bloc.dart';
 
-class ItineraryState extends Equatable {
-  const ItineraryState();
+class {{folder_name.pascalCase()}}State extends Equatable {
+  const {{folder_name.pascalCase()}}State();
 
   @override
   List<Object?> get props => [];
