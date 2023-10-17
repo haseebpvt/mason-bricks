132e7bb54b4a2406328554b5ea7e1b42d3140f2d
Abdul Haseeb <haseeb.a@2basetechnologies.com>
2023-10-17 23:33:47 +0530

    Added comment

--- screen/__brick__/{{folder_name}}/bloc/{{folder_name}}_bloc.dart
+++ screen/__brick__/{{folder_name}}/bloc/{{folder_name}}_bloc.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
-import 'package:core/core.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 
 part '{{folder_name}}_event.dart';
@@ -10,6 +9,6 @@ part '{{folder_name}}_state.dart';
 
 class {{folder_name.pascalCase()}}Bloc extends Bloc<{{folder_name.pascalCase()}}Event, {{folder_name.pascalCase()}}State> {
   {{folder_name.pascalCase()}}Bloc() : super(const {{folder_name.pascalCase()}}State()) {
-    
+    // on<LoadDataEvent>(_loadDataEvent);
   }
 }
